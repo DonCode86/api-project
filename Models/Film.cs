f72726ffcfbdee4e86442afdc0f6d6f783f04ff7
DonCode <99022919+DonCode86@users.noreply.github.com>
2023-01-23 15:43:42 +0100

    add-validations

--- Models/Film.cs
+++ Models/Film.cs
@@ -2,18 +2,46 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using StackExchange.Redis;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+using StringLengthAttribute = System.ComponentModel.DataAnnotations.StringLengthAttribute;
 
 namespace api_project.Models
 {
     public class Film
     {
-        public int Id { get; set; }
         [Required]
-        [StringLength(30)]
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "You have to insert the title")]
+        [StringLength(40)]
         public string? Title { get; set; }
-
+        [Required(ErrorMessage = "You have to insert the genre")]
+        [StringLength(40)]
         public string? Genre { get; set; }
-
+        [Required]
+        [Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true), DataType(DataType.Date, ErrorMessage = "Invalid date format")]
+        [DateLessThanOrEqualToToday]
         public DateTime ReleaseDate { get; set; }
+    }
+    public class DateLessThanOrEqualToToday : ValidationAttribute
+    {
+        public override string FormatErrorMessage(string name)
+        {
+            return "Date value should not be a future date";
+        }
+
+        protected override ValidationResult IsValid(object objValue, ValidationContext validationContext)
+        {
+            var dateValue = objValue as DateTime? ?? new DateTime();
+
+            if (dateValue.Date > DateTime.Now.Date)
+            {
+                return new ValidationResult(FormatErrorMessage(validationContext.DisplayName));
+            }
+            return ValidationResult.Success;
+        }
+
     }
 }
