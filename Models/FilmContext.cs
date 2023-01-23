1f207898a2347772eb955b45574673d75412775f
DonCode <99022919+DonCode86@users.noreply.github.com>
2023-01-23 10:59:14 +0100

    Aggiungere i file di progetto.

---
+++ Models/FilmContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api_project.Models
+{
+    public class FilmContext : DbContext
+    {
+        public FilmContext (DbContextOptions<FilmContext> options) : base(options) 
+        { 
+        }
+
+        DbSet<Film> Films { get; set; } = null!;
+    }
+}
